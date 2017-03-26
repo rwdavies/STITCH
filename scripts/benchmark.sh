a3cb9bdf3373849b0ba22b539f0c6973394b1291
rwdavies <robertwilliamdavies@gmail.com>
2017-03-26 20:00:56 +0100

    Generalize removal of STITCH R librari during benchmarking

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -8,13 +8,17 @@ script_dir=`dirname "$0"`
 cd "${script_dir}"/../
 STITCH_HOME=`pwd`
 export PATH=`pwd`/:${PATH}
+mkdir benchmark-results
 
 # 1.2.4 1.2.9 
 for version in 1.3.0
 do
     cd ${STITCH_HOME}
     echo start $version
-    rm -r -f /homes/rdavies/R/x86_64-redhat-linux-gnu-library/3.3/00LOCK-STITCH
+    # I think below should work on other machines
+    # Basically want the user location for libraries
+    r_libs=`R --slave -e ".libPaths()[1]" | awk '{print substr($0, 5, 1000)}' | tr -d '"'`
+    rm -r -f ${r_libs}/00LOCK-STITCH
     echo ${STITCH_HOME}/releases/STITCH_${version}.tar.gz
     R CMD INSTALL ${STITCH_HOME}/releases/STITCH_${version}.tar.gz
     cd ${STITCH_HOME}/test-data/mouse_data/
@@ -24,8 +28,8 @@ do
 	export OUTPUTDIR=${STITCH_HOME}/test-results/benchmark_${use}_${version}/
 	export N_CORES=1
 	export TITLE=${use}_${version}
-	export OUTPUT_PLOT=${STITCH_HOME}/benchmark_${use}_${version}.pdf
-	/usr/bin/time -v ${STITCH_HOME}/scripts/profile.R 2>&1 | tee ${STITCH_HOME}/time_${use}_${version}.txt
+	export OUTPUT_PLOT=${STITCH_HOME}/benchmark-results/benchmark_${use}_${version}.pdf
+	/usr/bin/time -v ${STITCH_HOME}/scripts/profile.R 2>&1 | tee ${STITCH_HOME}/benchmark-results/time_${use}_${version}.txt
     done
 done
 
