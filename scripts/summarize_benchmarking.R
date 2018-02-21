9b5d37bb71232368e02251a3198dc98ce3c78e78
rwdavies <robertwilliamdavies@gmail.com>
2018-02-21 16:34:19 +0000

    Make summary benchmarking available from anywhere

--- scripts/summarize_benchmarking.R
+++ scripts/summarize_benchmarking.R
@@ -3,8 +3,18 @@
 ## Script to summarize benchmarking runs
 
 library("knitr")
-STITCH_home <- "/data/smew1/rdavies/stitch_development/STITCH_github_latest/STITCH/"
+## change directory to one up from scripts, no matter how this was called
+args <- commandArgs(trailingOnly = FALSE)
+for(key in c("--file=", "--f=")) {
+    i <- substr(args, 1, nchar(key)) == key
+    if (sum(i) == 1) {
+        script_dir <- dirname(substr(args[i], nchar(key) + 1, 1000))
+        setwd(file.path(script_dir, "../"))
+    }
+}
+STITCH_home <- getwd()
 setwd(STITCH_home)
+print(getwd())
 
 knitr::knit('benchmarks/summarize_benchmarking.Rmd', output = "benchmarks/summarize_benchmarking.md")
 system("rsync -av benchmarks/summarize_benchmarking.md florence:~/")
