747c8cc085712e64b91c68bf462eefd4b768d7b8
rwdavies <robertwilliamdavies@gmail.com>
2018-06-21 15:23:08 +0100

    Wording changes in test-examples

--- scripts/test-examples.sh
+++ scripts/test-examples.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-# Run acceptance tests on installed version of STITCH
-# Technically, the "unit" tests within STITCH include both unit and acceptance tests
+# Run examples tests on installed version of STITCH
+# STITCH has unit and acceptance tests
 # These are more medium scale, real world examples
-# They are useful as they identify less obvious bugs, check performance
+# They are useful as they identify less obvious bugs, check performance, etc
 # Ideally, a more formal check would take on the output (with seeds) to
 # ensure results are invariant and/or confirm results against validation
 
