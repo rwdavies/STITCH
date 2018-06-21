4d8fa4fd7d9c248deb831857e08ead6fcfe36d64
rwdavies <robertwilliamdavies@gmail.com>
2018-06-21 14:46:19 +0100

    Add proftools to dependencies

--- scripts/install-r-dependencies.R
+++ scripts/install-r-dependencies.R
@@ -1,19 +1,10 @@
 #!/usr/bin/env Rscript
 
-if (!suppressPackageStartupMessages(require("Rcpp")))
-    install.packages("Rcpp", repos="http://cran.rstudio.com/")
-if (!suppressPackageStartupMessages(require("RcppArmadillo")))
-    install.packages("RcppArmadillo", repos="http://cran.rstudio.com/")
-if (!suppressPackageStartupMessages(require("optparse")))
-    install.packages("optparse", repos="http://cran.rstudio.com/")
-if (!suppressPackageStartupMessages(require("devtools")))
-    install.packages("devtools", repos="http://cran.rstudio.com/")
-if (!suppressPackageStartupMessages(require("testthat")))
-    install.packages("testthat", repos="http://cran.rstudio.com/")
-if (!suppressPackageStartupMessages(require("roxygen2")))
-    install.packages("roxygen2", repos="http://cran.rstudio.com/")
-if (!suppressPackageStartupMessages(require("data.table")))
-    install.packages("data.table", repos="http://cran.rstudio.com/")
+required_packages <- c("proftools", "Rcpp", "RcppArmadillo", "optparse", "devtools", "testthat", "roxygen2", "data.table")
+for(package in required_packages) {
+    if (!suppressPackageStartupMessages(require(package)))
+        install.packages(package, repos="http://cran.rstudio.com/")
+}
 if (!suppressPackageStartupMessages(require("rrbgen")))
     install.packages("https://github.com/rwdavies/rrbgen/raw/master/releases/rrbgen_0.0.1.tar.gz", repos=NULL)
 
