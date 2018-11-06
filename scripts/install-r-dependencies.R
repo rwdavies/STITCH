fa074e8e6c80557696b5310f21dedbb3abf3cece
rwdavies <robertwilliamdavies@gmail.com>
2018-11-06 14:49:15 -0500

    Throw error if do not install R package properly

--- scripts/install-r-dependencies.R
+++ scripts/install-r-dependencies.R
@@ -3,7 +3,11 @@
 required_packages <- c("proftools", "Rcpp", "RcppArmadillo", "optparse", "devtools", "testthat", "roxygen2", "data.table")
 for(package in required_packages) {
     if (!suppressPackageStartupMessages(require(package, character.only = TRUE))) {
-        install.packages(package, repos="http://cran.rstudio.com/")
+        out <- install.packages(package, repos="http://cran.rstudio.com/")
+        out <- require(package)
+        if (!out) {
+            stop(paste0("Failed to install package:", package))
+        }
     }
 }
 if (!suppressPackageStartupMessages(require("rrbgen")))
