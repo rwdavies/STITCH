648fac114dcfde38ba709cc236ced199dfacef9a
rwdavies <robertwilliamdavies@gmail.com>
2020-03-18 12:29:00 +0000

    switch back print_message to message as it should be

--- STITCH/R/functions.R
+++ STITCH/R/functions.R
@@ -5048,7 +5048,7 @@ print_message <- function(x, include_mem = FALSE) {
     } else {
         mem <- ""
     }
-    print(
+    message(
         paste0(
             "[", format(Sys.time(), "%Y-%m-%d %H:%M:%S"), "] ", mem, x
         )
