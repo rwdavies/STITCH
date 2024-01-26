b9060cde4bd31368e4691f4b8f509d905e76ee31
rwdavies <robertwilliamdavies@gmail.com>
2024-01-26 20:31:29 +0000

    update VCF comparison paths

--- scripts/test-compare-vcf.sh
+++ scripts/test-compare-vcf.sh
@@ -11,14 +11,16 @@ export PATH=`pwd`/:${PATH}
 
 if [ ! -e megamuga_2018_06_21.RData ]
 then
-    curl -k -O https://www.well.ox.ac.uk/~rwdavies/megamuga_2018_06_21.RData
+    #curl -k -O https://www.well.ox.ac.uk/~rwdavies/megamuga_2018_06_21.RData
+    curl -k -O https://www.chg.ox.ac.uk/~rwdavies/megamuga_2018_06_21.RData
 fi
 
 mkdir -p test-data/mouse_data/
 cd test-data/mouse_data/
 if [ ! -e STITCH_example_2016_05_10.tgz ]
 then
-    curl -k -O https://www.well.ox.ac.uk/~rwdavies/ancillary/STITCH_example_2016_05_10.tgz
+    #curl -k -O https://www.well.ox.ac.uk/~rwdavies/ancillary/STITCH_example_2016_05_10.tgz
+    curl -k -O https://www.chg.ox.ac.uk/~rwdavies/ancillary/STITCH_example_2016_05_10.tgz
 fi
 tar -xzf STITCH_example_2016_05_10.tgz
 cd ../../
